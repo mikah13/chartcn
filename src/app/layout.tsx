24b12d4930e126bc61d03ef25a9965e865b502f4
mikah13 <anhminhhoang13@gmail.com>
2023-12-16 08:26:39 +0000

    feat: added next-theme and theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next';
 import { GeistSans } from 'geist/font/sans';
-import { GeistMono } from 'geist/font/mono';
+// import { GeistMono } from 'geist/font/mono';
 
 import './globals.css';
+import { cn } from '@/lib/utils';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +16,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body className={GeistSans.className}>{children}</body>
+    <html lang='en' suppressHydrationWarning>
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          GeistSans.className
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
