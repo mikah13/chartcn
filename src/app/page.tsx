f9f871644daf21748ce5b1338b206f611788817a
mikah13 <anhminhhoang13@gmail.com>
2023-12-17 09:09:49 +0000

    chore: run format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,32 @@
-import {
-  PageHeader,
-  PageHeaderDescription,
-  PageHeaderHeading,
-} from '@/components/PageHeader';
-import AreaChart from '@/components/chart/AreaChart';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import Image from 'next/image';
+import { PageHeader, PageHeaderDescription, PageHeaderHeading } from '@/components/PageHeader'
+import AreaChart from '@/components/chart/AreaChart'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <div className='container'>
-      <section className='relative flex flex-col items-start md:flex-row md:items-center '>
-        <PageHeader className='relative pb-4 md:pb-8 lg:pb-12'>
-          <PageHeaderHeading>ChartCN</PageHeaderHeading>
-          <PageHeaderDescription>
-            A collection of charts for your dashboard built with ShadCN and
-            recharts
-          </PageHeaderDescription>
-        </PageHeader>
-      </section>
+	return (
+		<div className="container">
+			<section className="relative flex flex-col items-start md:flex-row md:items-center ">
+				<PageHeader className="relative pb-4 md:pb-8 lg:pb-12">
+					<PageHeaderHeading>ChartCN</PageHeaderHeading>
+					<PageHeaderDescription>
+						A collection of charts for your dashboard built with ShadCN and recharts
+					</PageHeaderDescription>
+				</PageHeader>
+			</section>
 
-      <section>
-        <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-4'>
-          <Card className='col-span-2'>
-            <CardHeader>
-              <CardTitle>Area Chart</CardTitle>
-            </CardHeader>
-            <CardContent className='pl-2'>
-              <AreaChart />
-            </CardContent>
-          </Card>
-        </div>
-      </section>
-    </div>
-  );
+			<section>
+				<div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+					<Card className="col-span-2">
+						<CardHeader>
+							<CardTitle>Area Chart</CardTitle>
+						</CardHeader>
+						<CardContent className="pl-2">
+							<AreaChart />
+						</CardContent>
+					</Card>
+				</div>
+			</section>
+		</div>
+	)
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,29 @@
-import { cn } from '@/lib/utils';
-import Link from 'next/link';
-import { ThemeToggle } from './ThemeToggle';
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import { ThemeToggle } from './ThemeToggle'
 
-import React from 'react';
-import { Button } from './ui/button';
-import { Icons } from './Icons';
+import React from 'react'
+import { Button } from './ui/button'
+import { Icons } from './Icons'
 
 const Header = () => {
-  return (
-    <header className='sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
-      <div className='container flex h-14 items-center'>
-        <div className='flex flex-1 items-center justify-between space-x-2 md:justify-end'>
-          <nav className='flex items-center'>
-            <Link href={'/'} target='_blank' rel='noreferrer'>
-              <Button size='icon' variant='ghost'>
-                <Icons.gitHub className='h-5 w-5' />
-              </Button>
-            </Link>
+	return (
+		<header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+			<div className="container flex h-14 items-center">
+				<div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
+					<nav className="flex items-center">
+						<Link href={'/'} target="_blank" rel="noreferrer">
+							<Button size="icon" variant="ghost">
+								<Icons.gitHub className="h-5 w-5" />
+							</Button>
+						</Link>
 
-            <ThemeToggle />
-          </nav>
-        </div>
-      </div>
-    </header>
-  );
-};
+						<ThemeToggle />
+					</nav>
+				</div>
+			</div>
+		</header>
+	)
+}
 
-export default Header;
+export default Header
